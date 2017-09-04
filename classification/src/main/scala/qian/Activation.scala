ab51924d199ede4b9aa69f32ee74a4755b3c1de6
qianwei <752004979@qq.com>
2017-09-04 19:05:26 +0800

    ~

--- classification/src/main/scala/qian/Activation.scala
+++ classification/src/main/scala/qian/Activation.scala
@@ -1,10 +1,14 @@
 package qian
 
+import breeze.linalg.DenseVector
+
 /**
   * Created by qianwei on 2017/9/4.
   */
 object Activation {
 
-  def sigmoid(z: Double): Unit = 1 / (1 + scala.math.exp(-z))
+  def sigmoid(z: Double): Double = 1 / (1 + scala.math.exp(-z))
+
+  def sigmoid(z: DenseVector[Double]) = null
 
 }
