5a4f140ce99b4e479ba1d5407dee398fb17c85bf
qianwei <752004979@qq.com>
2017-09-04 16:18:32 +0800

    ~

--- autodiff/src/main/scala/qian/node/MyNode.scala
+++ autodiff/src/main/scala/qian/node/MyNode.scala
@@ -5,15 +5,19 @@ import scala.collection.mutable
 /**
   * 运算节点
   */
-class MyNode extends Serializable{
+class MyNode extends Serializable {
+
   val input = new mutable.ArrayBuffer[MyNode]()
-  val op = null
+  var op: qian.op.Operation = _
   val const_attr = null
-  var name = ""
+  var name: String = _
 
   def +(other: MyNode) {
     if (other.isInstanceOf[MyNode])
       null
   }
 
+  def :*(other: MyNode): MyNode = {
+    null
+  }
 }
--- autodiff/src/main/scala/qian/op/MulOp.scala
+++ autodiff/src/main/scala/qian/op/MulOp.scala
@@ -1,5 +1,72 @@
 package qian.op
 
+import qian.node.MyNode
+
+import scala.collection.mutable
+import breeze.linalg.DenseVector
+
+/**
+  * 乘法操作, 向量相乘
+  * 构造函数私有化
+  */
+class MulOp private extends Operation {
+
+  /**
+    * 计算内集
+    * @param node
+    * @param inputValArr
+    * @return
+    */
+  def compute(node: MyNode, inputValArr: mutable.ArrayBuffer[DenseVector[Double]]): Double = {
+    assert(inputValArr.length == 2)
+    inputValArr(0).t * inputValArr(1)  // 两个向量相乘, 返回的是标量
+  }
+
+  /**
+    * 关于MyNode符号重载
+    * :* 表示求内集
+    * *  表示element wise 相乘
+    * @param node
+    * @param outputGrad
+    * @return
+    */
+
+  def gradient(node: MyNode, outputGrad: MyNode): mutable.ArrayBuffer[MyNode] = {
+    val r = new mutable.ArrayBuffer[MyNode]()
+    r += node.input(1) :* outputGrad
+    r += node.input(0) :* outputGrad
+    r
+  }
+
+  def apply(nodeA: MyNode, nodeB: MyNode): MyNode = {
+    val newNode = super.apply()
+    newNode.op = this
+    newNode.name = s"${nodeA.name} :* ${nodeB.name}"
+    newNode
+  }
+}
+
+
 object MulOp {
 
+  var instance: MulOp = _
+
+  def getInstance(): MulOp = {
+    this.synchronized {
+      if (instance == null){
+        println("first create")
+        instance = new MulOp()
+      }
+      instance
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val mlop = MulOp.getInstance()
+    val mlop2 = MulOp.getInstance()
+    println(mlop == mlop2)
+
+
+  }
 }
